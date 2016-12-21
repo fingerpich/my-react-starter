65b660f769922cc073c73c5803f0b3509cf7b14a
mojtaba <mojtaba.zarei@ronash.co>
2016-12-21 17:40:12 +0330

    make stateless component

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import reactDom from 'react-dom';
 import { createStore } from 'redux'
-import FirstComponent from './firstComponent/firstComponent.jsx';
-import Counter from './counter/counter.jsx';
 import counterReducer from './counter/counterReducer.jsx';
+import Counter from './counter/counter.jsx';
 
+const render=() => {
+    reactDom.render(
+        <Counter value={counterStore.getState()}
+                 onIncrement={() => {counterStore.dispatch({type:'INCREMENT'})}}
+                 onDecrement={() => {counterStore.dispatch({type:'DECREMENT'})}}
+        />,
+        document.getElementById('app')
+    );
+};
 const counterStore = createStore(counterReducer);
-
-class App extends React.Component {
-    constructor(props){
-        super(props);
-        counterStore.subscribe(this.render);
-    }
-    render () {
-        return (
-            <div>
-                <p> Hello React!</p>
-                <FirstComponent></FirstComponent>
-                
-                <Counter
-                    value={counterStore.getState()}
-                    onIncrement={() => counterStore.dispatch({ type: 'INCREMENT' })}
-                    onDecrement={() => counterStore.dispatch({ type: 'DECREMENT' })}
-                />
-            </div>
-        );
-    }
-}
-
-reactDom.render(<App/>, document.getElementById('app'));
+counterStore.subscribe(render);
+render();
